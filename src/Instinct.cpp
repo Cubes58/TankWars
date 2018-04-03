876f5ce2605087817e78a06560dcca6fb6ecbfe1
Sam <samknight4321@gmail.com>
2018-04-03 18:49:30 +0100

    Revert "*"
    This reverts commit 32090171885848d59086cd8b7e22c393d262e08e.

--- src/Instinct.cpp
+++ src/Instinct.cpp
@@ -2,12 +2,8 @@
 
 Instinct::Instinct() {
 	clearMovement(); //Clear some weird movement bug
-	//Node *tempNode = &m_Graph->getNode(sf::Vector2u(25, 20));
-	Node *tempNode1 = &m_Graph->getNode(sf::Vector2u(5, 15));
-	//Node *tempNode2 = &m_Graph->getNode(sf::Vector2u(10, 20));
-	//m_Path.push_back(tempNode);
-	m_Path.push_back(tempNode1);
-	//m_Path.push_back(tempNode2);
+	Node *tempNode = &m_Graph->getNode(sf::Vector2u(1, 1));
+	m_Path.push_back(tempNode);
 	m_eMainState = MainStates::Attacking;
 	m_eAttackingState = AttackingStates::Locating;
 }
@@ -85,7 +81,7 @@ void Instinct::drive()
 		float dTankAngle = pos.getTh();
 		float dDeltaAngle = dTankAngle - dTargetAngle;
 		bool facingTarget = (dDeltaAngle < rotPrecision && dDeltaAngle > -rotPrecision);
-		bool finishedDriving = (m_Path.size() == 0);
+		bool finishedDriving = m_Path.size() == 0;
 
 		if (facingTarget && !finishedDriving)
 		{
@@ -114,10 +110,6 @@ void Instinct::drive()
 			if (m_Path.size() != 0)
 			{
 				m_TargetNode = m_Path.back();
-			}
-			else
-			{
-				stop();
 			}
 		}
 	}
